8d77e96de7bab86a6ed6c4bf9308c797b862675b
Tim Nederhoff <tim.nederhoff@columbo.nl>
2019-09-24 21:02:26 +0200

    recent work experience

---
+++ src/app/cvparts/preview/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import cvData from '../../../../assets/cv_tim_nederhoff_nl.json';
+
+@Component({
+  selector: 'app-footer',
+  template: '<footer>{{basics.email}} | {{basics.phone}} | {{basics.location.address}}, {{basics.location.postalCode}} {{basics.location.city}} | {{linkedInUrl}}</footer>'
+})
+export class FooterComponent {
+
+  basics = cvData.basics;
+  linkedInUrl = this.basics.profiles.filter(p => p.network === 'LinkedIn')[0].url;
+  constructor() { }
+
+}
--- src/app/cvparts/preview/titlepage/titlepage.component.ts
+++ src/app/cvparts/preview/titlepage/titlepage.component.ts
@@ -1,13 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-titlepage',
   templateUrl: './titlepage.component.html',
   styleUrls: ['./titlepage.component.css']
 })
-export class TitlepageComponent {
+export class TitlepageComponent implements OnInit {
 
   @Input() resumeData;
+  @Input() selectedWork;
+  basics: any;
   constructor() { }
+
+  ngOnInit(): void {
+    this.basics = this.resumeData.basics;
+  }
+
+  recentWork() {
+    return this.selectedWork.sort((o, a) => {
+      return new Date(a.value.startDate).getTime() - new Date(o.value.startDate).getTime();
+    }).slice(0, 4);
+  }
 
 }
